92410eba31a5ed3a8f91fd2a930303f3efb7dc4f
sreelekha-24323 <inf24323@adobe.com>
2026-09-02 18:44:27 +0530

    added style changes for promo-grid

--- blocks/promo-grid/promo-grid.js
+++ blocks/promo-grid/promo-grid.js
@@ -1,26 +1,29 @@
 export default async function decorate(block) {
-  const jsonUrl = block.querySelector('a')?.href
-    || block.textContent.trim();
+    const jsonUrl = block.querySelector('a')?.href
+        || block.textContent.trim();
 
-  const response = await fetch(`eds-ecommerce/data${jsonUrl}`);
-  const json = await response.json();
+    const response = await fetch(`eds-ecommerce/data${jsonUrl}`);
+    const json = await response.json();
 
-  const cards = json.data.map((category) => `
-    <a href="${category.path}">
-      <div class="promo-card-image">
-        ${category.image}
-      </div>
+    const type=jsonUrl.includes('categories') ? 'categories' : 'products';
+
+    const cards = json.data.map((item) => `
+  <li class="promo-card">
+      <img class="promo-card-image" src="${item.image}" alt="${item.category}">
 
       <div class="promo-card-content">
-        <h3>${category.name}</h3>
-        <p>${category.description}</p>
+        <h3>${item.category || item.brand}</h3>
+
+        ${type==="products" ? `<p>${item.title}</p>` : ''}
+        ${type==="products" ? `<p>&dollar;${item.price}</p>` : ''}
+        ${type==="products" ? `<button class="promo-card-cta">Add to Cart</button>` : ''}
       </div>
-    </a>
+    </li>
   `).join('');
 
-  block.innerHTML = `
-    <div class="promo-grid-container">
+    block.innerHTML = `
+    <ul class="promo-grid-list">
       ${cards}
-    </div>
+    </ul>
   `;
 }
