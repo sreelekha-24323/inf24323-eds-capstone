179ce41827ad2944e2d1c4e6e4cf931284a12d24
sreelekha-24323 <inf24323@adobe.com>
2026-09-04 13:58:10 +0530

    added related product js file

---
+++ blocks/related-products/related-products.js
@@ -0,0 +1,38 @@
+import { renderProductTeaser }
+  from '../product-teaser/product-teaser.js';
+
+export default async function decorate(block) {
+  const response = await fetch('/query-index.json');
+  const json = await response.json();
+
+  const currentPath = window.location.pathname;
+
+  const currentProduct = json.data.find(
+    (item) => item.path === currentPath
+  );
+
+  if (!currentProduct) return;
+
+  const relatedProducts = json.data
+    .filter((item) =>
+      item.type === 'product'
+      && item.category === currentProduct.category
+      && item.path !== currentPath)
+    .slice(0, 4);
+
+  block.innerHTML = `
+    <div class="related-products-wrapper">
+
+      <h2>Related Products</h2>
+
+      <div class="related-products-grid">
+
+        ${relatedProducts
+          .map(renderProductTeaser)
+          .join('')}
+
+      </div>
+
+    </div>
+  `;
+}
